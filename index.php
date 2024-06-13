e3f271b8078956cf13d892c730a751e09287ba8f
Kadu <kadu2002old@gmail.com>
2024-06-13 14:59:53 -0300

    Primeiro contato com php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo PHP</title>
+</head>
+<body>
+
+<?php
+// Definindo uma variável com uma mensagem
+$mensagem = "Olá, mundo! Este é um exemplo simples de PHP.";
+
+// Exibindo a mensagem na tela
+echo "<h1>$mensagem</h1>";
+?>
+
+</body>
+</html>
