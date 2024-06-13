5e7c99f8a2aa495144df24b15a395baccf0a2471
Kadu <kadu2002old@gmail.com>
2024-06-13 15:17:24 -0300

    First contact

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 <body>
 
 <?php
-// Definindo uma variável com uma mensagem
+
 $mensagem = "Olá, mundo! Este é um exemplo simples de PHP.";
 
-// Exibindo a mensagem na tela
+
 echo "<h1>$mensagem</h1>";
 ?>
 
